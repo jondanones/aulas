339b7dbbe1e2ebecad11e4fba6d585a08f57df39
John <john.sama64@hotmail.com>
2020-07-08 19:20:17 -0300

    teste

---
+++ classe4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace classe4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + funcionario);
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+
+            int aumento = int.Parse(Console.ReadLine());
+            funcionario.AumentarSalario(aumento);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + funcionario);
+
+
+
+        }
+    }
+}
